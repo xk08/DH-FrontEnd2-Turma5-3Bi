e9b697b5af5087a218849b85560ea57d243481dd
XK08 <gronytzki@gmail.com>
2022-03-22 22:18:52 -0300

    Aula 13, dia 22-03-22, formulários II, arquivos feitos em aula

--- Aula13--Formulários_II/scripts/script_aula13.js
+++ Aula13--Formulários_II/scripts/script_aula13.js
@@ -1,3 +1,4 @@
+/* Capturando as informações do formulário */
 let nomeSobrenome = document.getElementById("nome");
 let idade = document.getElementById("idade");
 let biografia = document.getElementById("biografia");
@@ -9,6 +10,7 @@ let email = document.getElementById("email");
  */
 let botaoSalvar = document.getElementById("salvar");
 
+/* Criando um objeto para representar o usuário e instaciando com valores "" */
 let objetoUsuario = {
   nome: "",
   sobrenome: "",
@@ -17,6 +19,7 @@ let objetoUsuario = {
   altura: "",
   email: "",
 };
+
 
 botaoSalvar.addEventListener("click", function (event) {
   //Impede que a página seja atualizada
@@ -28,8 +31,70 @@ botaoSalvar.addEventListener("click", function (event) {
   objetoUsuario.altura = altura.value;
   objetoUsuario.email = email.value;
 
-  carregaInformacoesEmTela(objetoUsuario);
+  objetoUsuario.nome = retiraEspacosNomeUsuario(nomeSobrenome.value);
+
+  let nomeCompletoUsuario = separaNomeUsuario(objetoUsuario.nome);
+  console.log(nomeCompletoUsuario);
+
+  objetoUsuario.nome = nomeCompletoUsuario[0];
+  objetoUsuario.sobrenome = nomeCompletoUsuario[1];
+
+  if (validacaoEstaOk) {
+    carregaInformacoesEmTela(objetoUsuario);
+  }
 });
+
+let validacaoEstaOk = false;
+
+nomeSobrenome.addEventListener("blur", function () {
+  let nomeValidacao = document.getElementById("nomeValidacao");
+
+  if (nomeSobrenome.value != "") {
+    console.log("Está preenchido");
+
+    nomeValidacao.innerText = "";
+    nomeSobrenome.style.backgroundColor = "#ffff";
+    botaoSalvar.removeAttribute("disabled");
+    validacaoEstaOk = true;
+
+  } else {
+    console.log(" Não Está preenchido");
+    nomeValidacao.style.color = "#E92323";
+    nomeValidacao.innerText = "Campo é obrigatório";
+    nomeSobrenome.style.backgroundColor = "#DB7979BE";
+    botaoSalvar.setAttribute("disabled", true);
+    validacaoEstaOk = false;
+
+  }
+});
+
+email.addEventListener("blur", function () {
+
+  let emailValidacao = document.getElementById('emailValidacao');
+
+    /* Utilizando expressão regulares (RegExp)*/
+    if ( /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+      emailValidacao.style.color = "#E92323";
+      botaoSalvar.removeAttribute("disabled");
+      emailValidacao.innerText = "";
+      validacaoEstaOk = true;
+    } else {
+    emailValidacao.style.color = "#E92323";
+    emailValidacao.innerText = "O e-mail informado é inválido";
+    nomeSobrenome.style.backgroundColor = "#DB7979BE";
+    botaoSalvar.setAttribute("disabled", true);
+    validacaoEstaOk = false;
+    }
+    
+});
+
+function retiraEspacosNomeUsuario(nomeUsuarioRecebido) {
+  return nomeUsuarioRecebido.trim();
+}
+
+function separaNomeUsuario(nomeUsuarioRecebido) {
+  return nomeUsuarioRecebido.split(" ");
+}
 
 function carregaInformacoesEmTela(objetoUsuarioRecebido) {
   //Captura as variáveis, para exibir as novas informações em tela
@@ -39,6 +104,8 @@ function carregaInformacoesEmTela(objetoUsuarioRecebido) {
   let alturaDom = document.getElementById("alturaUsuario");
   let idadeDom = document.getElementById("idadeUsuario");
   let emailDom = document.getElementById("emailUsuario");
+
+  console.log(objetoUsuarioRecebido.nome);
 
   nomeDom.innerText = objetoUsuarioRecebido.nome;
   sobrenomeDom.innerText = objetoUsuarioRecebido.sobrenome;
